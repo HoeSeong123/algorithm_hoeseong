909bffbad6035ddb8b141a18f7f55d63d7d3219f
HoeSeong123 <ghltjd3@gmail.com>
2023-09-01 14:09:36 +0900

    프로그래머스 181187 두 원 사이의 정수 쌍

--- src/main/java/com/ll/프로그래머스/레벨2/p181187/Solution.java
+++ src/main/java/com/ll/프로그래머스/레벨2/p181187/Solution.java
@@ -4,26 +4,18 @@ public class Solution {
     public long solution(int r1, int r2) {
         long answer = 0;
 
-        for(int i = 1; i < r2; i++) {
-            for(int j = 1; j < i; j++) {
-                double num = Math.sqrt(Math.pow(i, 2) + Math.pow(j, 2));
+        for(long i = 1; i < r2; i++){
+            long y1 = (long)Math.sqrt(Math.pow(r1, 2) - Math.pow(i, 2));
+            long y2 = (long)Math.sqrt(Math.pow(r2, 2) - Math.pow(i, 2));
+            answer += y2 - y1;
 
-                if(num >= r1 && num <= r2) answer++;
+            if(Math.pow(y1, 2) + Math.pow(i, 2) == Math.pow(r1, 2) && y1 != 0) {
+                answer++;
             }
         }
 
-
-        double tmp1 = (double)Math.pow(r1, 2) / 2;
-        double tmp2 = (double)Math.pow(r2, 2) / 2;
-
-        answer = answer * 8 + 4 * (r2 - r1 + 1) + 4 * ((long)Math.sqrt(tmp2) - (long)Math.sqrt(tmp1));
+        answer = 4 * answer + 4 * (r2 - r1 + 1);
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        long num = solution.solution(1,5);
-        System.out.println(num);
     }
 }
