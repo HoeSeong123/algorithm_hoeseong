6eae76aefb5cc1bafc51e25c1eea803583df153d
HoeSeong123 <ghltjd3@gmail.com>
2023-10-09 11:54:23 +0900

    백준 152996 시소 짝꿍

--- src/main/java/com/ll/프로그래머스/레벨2/p152996/Solution.java
+++ src/main/java/com/ll/프로그래머스/레벨2/p152996/Solution.java
@@ -4,24 +4,44 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution {
+import java.util.*;
+
+class Solution {
     public long solution(int[] weights) {
         long answer = 0;
+
         Arrays.sort(weights);
-        Map<Double, Integer> map = new HashMap<>();
-        for(int i : weights) {
-            double a = i*1.0;
-            double b = (i*2.0)/3.0;
-            double c = (i*1.0)/2.0;
-            double d = (i*3.0)/4.0;
-            if(map.containsKey(a)) answer += map.get(a);
-            if(map.containsKey(b)) answer += map.get(b);
-            if(map.containsKey(c)) answer += map.get(c);
-            if(map.containsKey(d)) answer += map.get(d);
-            map.put((i*1.0), map.getOrDefault((i*1.0), 0)+1);
+
+        int cnt = 0;
+        for(int i = 0; i < weights.length - 1; i++) {
+            if(i != 0) {
+                if(weights[i] == weights[i - 1]) {
+                    cnt--;
+                    answer += cnt;
+                    continue;
+                }
+            }
+
+            cnt = 0;
+
+            for(int j = i + 1; j < weights.length; j++) {
+                int num1 = weights[i];
+                int num2 = weights[j];
+
+                if(num1 == num2 ||
+                        num1 * 2 == num2 * 3 ||
+                        num1 * 2 == num2 * 4 ||
+                        num1 * 3 == num2 * 2 ||
+                        num1 * 3 == num2 * 4 ||
+                        num1 * 4 == num2 * 2 ||
+                        num1 * 4 == num2 * 3) {
+                    cnt++;
+                }
+            }
+
+            answer += cnt;
         }
 
         return answer;
-
     }
 }
